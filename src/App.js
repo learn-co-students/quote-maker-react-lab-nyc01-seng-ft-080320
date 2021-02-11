9247e8c27a95631a67246a095f1ae5f6f5a71fb2
royFloresNyc <royfloresnyc@gmail.com>
2021-02-10 20:08:20 -0500

    Connect  QuoteForm and QuoteCard to store

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import QuoteForm from './components/QuoteForm'
+import QuoteCard from './components/QuoteCard'
 
 class App extends Component {
+
+    renderQuotes(){
+        return this.props.quotes.map((quote, indx) => <QuoteCard key={indx} quote={quote}/>)
+    }
+
   render() {
+      console.log("APP STATE: ", this.props.quotes)
     return (
       <div className="container-fluid">
         <div className="row title justify-content-center" style={{ paddingTop: '12px' }}>
           <h1>Quote Maker</h1>
         </div>
         <hr />
-        {/*
-          TODO:
-
-          Render QuoteForm and Quotes Components
-         */}
+        <QuoteForm/>
+        {this.renderQuotes()}
       </div>
     );
   }
 }
 
-export default App;
+export default connect(state => ({ quotes: state.quotes }))(App);
--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,33 @@
 // TODO: Create action creators as defined in tests
+
+export const addQuote = quote => {
+    return {
+        type: 'ADD_QUOTE',
+        quote
+    }
+}
+
+export const removeQuote = quote => {
+    return {
+        type: 'REMOVE_QUOTE',
+        quote
+    }
+}
+
+export const upvoteQuote = quote => {
+    return {
+        type: 'UP_VOTE_QUOTE',
+        quote
+    }
+}
+
+export const downvoteQuote = quote => {
+    return {
+        type: 'DOWN_VOTE_QUOTE',
+        quote
+    }
+}
+
+
+
+
